4d9e373e002cd7339373b866c05336b646c8b481
praveen98331-art <praveen98331@gmail.com>
2025-08-12 11:51:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+TOKEN = "7912733347:AAFeuZDn544pkmd7bUwcRLSBfqjqxYkCnkU"
+CHANNEL_LINK = "https://t.me/+dt6vu048PjFjMjU1"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"💥 Welcome! Join our channel here:\n{CHANNEL_LINK}")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+print("Bot is running...")
+app.run_polling()
